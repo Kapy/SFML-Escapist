6ccfc6ce4f677d1607e1ef674e9452371dcbbcdb
Kapy <Kapy@users.noreply.github.com>
2014-12-30 15:46:40 +0100

    Update Sortie.h

--- Sortie.h
+++ Sortie.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "ObjInteractif.h"
+#include "Niveau.h"
 
 class Sortie : public ObjInteractif
 {
-
+	Niveau* pParent;
 public:
 
-	Sortie();
+	Sortie(Niveau* pParent) : pParent(pParent)
+	{}
+
 	~Sortie();
 
 private:
