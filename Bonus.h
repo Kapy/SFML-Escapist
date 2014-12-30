672e7175635cd5f5c6fa4c97cfe5ef464f72928d
Kapy <Kapy@users.noreply.github.com>
2014-12-30 15:46:09 +0100

    Update Bonus.h

--- Bonus.h
+++ Bonus.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "ObjInteractif.h"
+#include "Niveau.h"
 
 class Bonus : public ObjInteractif
 {
-
+	Niveau* pParent;
 public:
 
-	Bonus();
+	Bonus(Niveau* pParent) : pParent(pParent)
+	{}
+
 	~Bonus();
 
 private:
